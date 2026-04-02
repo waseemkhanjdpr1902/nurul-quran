bdbd33b864dcf3e753e15a32f39040c629c042b1
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-04-02 10:16:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This is the "Magic Switch": It tells Vercel to ignore 
-    // these errors and finish the build anyway.
+    // This tells Vercel to ignore those red error messages and finish the build
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This also ignores code formatting errors
+    // This ignores formatting warnings
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
